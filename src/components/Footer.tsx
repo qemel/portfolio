aa2cfb2a167e0a58ae8440f490208dadfa04efe0
Noth827 <99727161+Noth827@users.noreply.github.com>
2024-04-15 16:34:13 +0900

    :wrench: change statement

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <div className="mt-12">
       <footer className="bg-gray-900 text-white text-center py-4">
-        <p>© 2024 Portfolio</p>
+        <p>© 2024 Portfolio of Qemel</p>
       </footer>
     </div>
   );
